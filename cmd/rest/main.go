58298343ba51bbf282229e00b422f4abe2a3e665
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-07-15 12:27:11 +0700

    create api for upload image

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/xryar/golang-grpc-ecommerce/internal/handler"
 )
+
+func handleGetFileName(c *fiber.Ctx) error {
+	fileNameParam := c.Params("filename")
+	filePath := path.Join("storage", "product", fileNameParam)
+	if _, err := os.Stat(filePath); err != nil {
+		if os.IsNotExist(err) {
+			return c.Status(http.StatusNotFound).SendString("Not Found")
+		}
+
+		log.Println(err)
+		return c.Status(http.StatusInternalServerError).SendString("Internal Server Error")
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err)
+		return c.Status(http.StatusInternalServerError).SendString("Internal Server Error")
+	}
+
+	ext := path.Ext(filePath)
+	mimeType := mime.TypeByExtension(ext)
+
+	c.Set("Content-Type", mimeType)
+	return c.SendStream(file)
+}
 
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New())
+	app.Get("/storage/product/:filename", handleGetFileName)
 	app.Post("/product/upload", handler.UploadProductImageHandler)
 
 	app.Listen(":3000")
